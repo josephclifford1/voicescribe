156a6279bf06afedb23613672aed9ab851242b58
josephclifford1 <josephclifford1000@gmail.com>
2026-03-04 00:28:18 +0100

    fix CORS for production

--- voicescribe/server.js
+++ voicescribe/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(cors());
 app.use(express.json());
 
 app.post('/api/chat', async (req, res) => {
