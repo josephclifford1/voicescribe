bd8409ba7850af1696d70ebe428c283c488d4109
josephclifford1 <josephclifford1000@gmail.com>
2026-03-04 11:19:22 +0100

    revert to working model

--- voicescribe/server.js
+++ voicescribe/server.js
@@ -89,7 +89,7 @@ app.post('/api/chat', async (req, res) => {
         'Authorization': 'Bearer ' + process.env.GROQ_API_KEY,
       },
       body: JSON.stringify({
-        model: 'mixtral-8x7b-32768',
+        model: 'llama-3.3-70b-versatile',
         messages: [
           { role: 'system', content: enhancedSystem },
           ...messages
